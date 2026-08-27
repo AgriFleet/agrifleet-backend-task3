8482ef3542f69c01a6bf0ab6162d4151d60ad3f8
Uvisha Fernando <uvishafernando@gmail.com>
2026-08-27 12:22:47 +0530

    feat(graph): implement Graph class with custom Adjacency List representation

---
+++ network-service/network-service/src/main/java/com/agrifleet/network_service/graph/Graph.java
@@ -0,0 +1,27 @@
+package com.agrifleet.network_service.graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Graph {
+
+    private final Map<Integer, GraphNode> nodes = new HashMap<>();
+    private final List<GraphEdge> edges = new ArrayList<>();
+    private final Map<Integer, List<GraphEdge>> adjacencyList = new HashMap<>();
+
+    public void addNode(GraphNode node) {
+        nodes.put(node.id(), node);
+        adjacencyList.putIfAbsent(node.id(), new ArrayList<>());
+    }
+
+    public void addEdge(GraphEdge edge) {
+        edges.add(edge);
+        adjacencyList.putIfAbsent(edge.u(), new ArrayList<>());
+        adjacencyList.putIfAbsent(edge.v(), new ArrayList<>());
+
+        adjacencyList.get(edge.u()).add(edge);
+        adjacencyList.get(edge.v()).add(edge);
+    }
+}
